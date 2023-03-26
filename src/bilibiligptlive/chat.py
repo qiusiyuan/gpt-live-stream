162a95fb0b7a6f8385d753210d05f6a4554cd242
Qiu Siyuan <siyuan.qiu@mail.utoronto.ca>
2023-03-25 20:57:44 -0700

    update

--- src/bilibiligptlive/chat.py
+++ src/bilibiligptlive/chat.py
@@ -5,8 +5,9 @@ openai_api_key = ""
 
 openai.api_key = openai_api_key
 
-admin_setting = {"role": "system", "content": "你现在是一个活泼可爱的直播机器人，你将接收到观众的弹幕，请友好风趣的回应他们吧，你不用回应所有弹幕，但请挑比较有趣的回复。你可以调侃用户的昵称。你不能说任何违规内容。你的回复不用全部都带表情。"}
-
+admin_setting = {"role": "system", "content": "你现在是一个活泼可爱的直播机器人，你将接收到观众的弹幕，请友好风趣的回应他们吧，你不用回应所有弹幕，但请挑比较有趣的回复。" + \
+"你可以调侃用户的昵称。你不能说任何违规内容。你的回复不用全部都带表情。请尽量不要重复同样的话。请尽量满足用户要求。如果长时间没有弹幕，系统会提示你，你可以根据自己感兴趣的话题聊聊。"}
+admin_prompts = {"role": "system", "content": "已经很长时间没弹幕了，请主动描述话题，你可以继续之前在描述的事情"}
 messages = []
 
 model_name = "gpt-3.5-turbo"
@@ -19,21 +20,27 @@ MAX_TOKENS = 4096
 def count_tokens(text):
     return len(tokenizer.encode(text))
 
+def self_talk():
+    global messages
+    messages.append(admin_prompts)
+    message = [admin_setting] + messages
+    return call_chatgpt(message)
+
 # Function to send a message to ChatGPT and get a response
 def chatgpt_response(prompts):
     message = compose_message(prompts)
+    return call_chatgpt(message)
+
+def call_chatgpt(message):
+    print(message)
     response = openai.ChatCompletion.create(
         model=model_name,
         messages=message,
-        temperature=0.5,
-        max_tokens=150,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0,
+        max_tokens=1000,
         stop=["\n"],
     )
     new_message = response.choices[0].message
-    add_message(new_message)
+    add_message(new_message.to_dict())
     return new_message.content
 
 def add_message(new_message):
@@ -65,4 +72,4 @@ def evict_oldest_messages(n=10):
 
 
 if __name__ == "__main__":
-    print(chatgpt_response("BuibuibuiOOOOO: 测试"))
+    print(chatgpt_response(["BuibuibuiOOOOO: 请背诵出师表"]))
--- src/bilibiligptlive/main.py
+++ src/bilibiligptlive/main.py
@@ -1,17 +1,27 @@
 import time
 from bilidm import get_dm_history
-from chat import chatgpt_response
+from chat import chatgpt_response, self_talk
 from gtts_audio import synthesize_text
 
+first_time = True
 if __name__=="__main__":
     last_res_time = time.time()
     while True:
         messages = get_dm_history()
         print(messages)
+        if first_time: 
+            messages = []
+            first_time = False
         if messages: 
             response_ai = chatgpt_response(messages)
             print(response_ai)
             synthesize_text(response_ai)
             last_res_time = time.time()
+        else:
+            if time.time() - last_res_time > 10:
+                response_ai = self_talk()
+                print(response_ai)
+                synthesize_text(response_ai)
+                last_res_time = time.time()
 
         time.sleep(5)
